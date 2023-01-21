3953a15f45e8a708739b2a429ff4be9cecc4054e
Waterstar1 <waterstar12001@gmail.com>
2023-01-20 20:05:53 -0800

    Added unit test for csv combiner

--- csv-combiner/src/csvcombiner.py
+++ csv-combiner/src/csvcombiner.py
@@ -9,13 +9,24 @@ import pandas
 DIR = path.abspath(path.dirname(__file__) + "/../")
 
 
+def write_csv(dataframe: pandas.DataFrame, output):
+    """ Writes dataframe to output """
+    dataframe.to_csv(output, index=False, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL,
+                     lineterminator='\n')
+
+
+def read_csv(filepath: str):
+    """ Reads dataframe from filepath """
+    return pandas.read_csv(filepath, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL)
+
+
 def combine_csv(filepaths: list):
-    """ Combines csv files by header and outputs to stdout """
+    """ Combines csv file and returns resulting dataframe """
 
     dataframes = []
 
     for filepath in filepaths:
-        dataframe = pandas.read_csv(filepath, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL)
+        dataframe = read_csv(filepath)
         filename = path.basename(path.normpath(filepath))
 
         # add additional column to specify filename
@@ -24,7 +35,7 @@ def combine_csv(filepaths: list):
         dataframes.append(dataframe)
 
     combined_dataframe = pandas.concat(dataframes)
-    combined_dataframe.to_csv(sys.stdout, index=False, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL, lineterminator='\n')
+    return combined_dataframe
 
 
 def main():
@@ -33,7 +44,9 @@ def main():
     parser.add_argument('filepaths', metavar='N', type=str, nargs='+', help='a csv file to combine')
 
     args = parser.parse_args()
-    combine_csv(args.filepaths)
+
+    combined_dataframe = combine_csv(args.filepaths)
+    write_csv(combined_dataframe, sys.stdout)
 
 
 if __name__ == '__main__':
--- csv-combiner/test/test_csvcombiner.py
+++ csv-combiner/test/test_csvcombiner.py
@@ -1,3 +1,66 @@
-import src
+from src.csvcombiner import combine_csv
+from unittest.mock import patch
 
-from src import csvcombiner
+import unittest
+import pandas
+
+test_data_dict1 = {
+    "email_hash": ["a",
+                   "b",
+                   "c"
+                   ],
+    "category": ["Shirts",
+                 "Blouses",
+                 "\"Gingham\" Shorts"
+                 ]
+}
+
+test_data_dict2 = {
+    "email_hash": ["d",
+                   "e",
+                   "f"
+                   ],
+    "category": ["Shirts",
+                 "Blouses",
+                 "\"Gingham\" Shorts"
+                 ]
+}
+
+test_data_dict3 = {
+    "email_hash": ["a",
+                   "b",
+                   "c",
+                   "d",
+                   "e",
+                   "f"
+                   ],
+    "category": ["Shirts",
+                 "Blouses",
+                 "\"Gingham\" Shorts",
+                 "Shirts",
+                 "Blouses",
+                 "\"Gingham\" Shorts"
+                 ],
+    "filename": ["l.csv",
+                "l.csv",
+                "l.csv",
+                "r.csv",
+                "r.csv",
+                "r.csv"
+                 ],
+}
+
+
+class TestCvsCombiner(unittest.TestCase):
+
+    @patch('src.csvcombiner.read_csv')
+    def test_combine_csv(self, read_csv):
+        dataframe1 = pandas.DataFrame(test_data_dict1)
+        dataframe2 = pandas.DataFrame(test_data_dict2)
+
+        expected_dataframe = pandas.DataFrame(test_data_dict3)
+
+        read_csv.side_effect = [dataframe1, dataframe2]
+        actual_dataframe = combine_csv(["l.csv", "r.csv"])
+
+        self.assertEqual(expected_dataframe.to_string(index=False), actual_dataframe.to_string(index=False))
