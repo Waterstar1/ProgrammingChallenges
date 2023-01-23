971217841ccf77029281194672910b194096e133
Waterstar1 <waterstar12001@gmail.com>
2023-01-22 23:54:57 -0800

    Added additional unit tests for combine cvs with multiple files

--- csv-combiner/test/test_csvcombiner.py
+++ csv-combiner/test/test_csvcombiner.py
@@ -11,16 +11,25 @@ test_data_dict1 = {
 
 test_data_dict2 = {
     "email_hash": ["d", "e", "f"],
-    "category": ["Shirts", "Blouses", "\"Gingham\" Shorts"
+    "category": ["Purple", "Unicorns", "First"
                  ]
 }
 
-test_data_dict3 = {
+expected_data_dict_two_files = {
     "email_hash": ["a", "b", "c", "d", "e", "f"],
     "category": ["Shirts", "Blouses", "\"Gingham\" Shorts",
-                 "Shirts", "Blouses", "\"Gingham\" Shorts"],
+                 "Purple", "Unicorns", "First"],
     "filename": ["l.csv", "l.csv", "l.csv", "r.csv", "r.csv", "r.csv"],
 }
+
+expected_data_dict_multiple_files = {
+    "email_hash": ["a", "b", "c", "d", "e", "f", "a", "b", "c"],
+    "category": ["Shirts", "Blouses", "\"Gingham\" Shorts",
+                 "Purple", "Unicorns", "First",
+                 "Shirts", "Blouses", "\"Gingham\" Shorts"],
+    "filename": ["l.csv", "l.csv", "l.csv", "r.csv", "r.csv", "r.csv", "m.csv", "m.csv", "m.csv"],
+}
+
 
 
 class TestCvsCombiner(unittest.TestCase):
@@ -30,9 +39,22 @@ class TestCvsCombiner(unittest.TestCase):
         dataframe1 = pandas.DataFrame(test_data_dict1)
         dataframe2 = pandas.DataFrame(test_data_dict2)
 
-        expected_dataframe = pandas.DataFrame(test_data_dict3)
+        expected_dataframe = pandas.DataFrame(expected_data_dict_two_files)
 
         read_csv.side_effect = [dataframe1, dataframe2]
         actual_dataframe = combine_csv(["l.csv", "r.csv"])
+
+        self.assertEqual(expected_dataframe.to_string(index=False), actual_dataframe.to_string(index=False))
+
+    @patch('src.csvcombiner.read_csv')
+    def test_combine_csv_multiple_files(self, read_csv):
+        dataframe1 = pandas.DataFrame(test_data_dict1)
+        dataframe2 = pandas.DataFrame(test_data_dict2)
+        dataframe3 = pandas.DataFrame(test_data_dict1)
+
+        expected_dataframe = pandas.DataFrame(expected_data_dict_multiple_files)
+
+        read_csv.side_effect = [dataframe1, dataframe2, dataframe3]
+        actual_dataframe = combine_csv(["l.csv", "r.csv", "m.csv"])
 
         self.assertEqual(expected_dataframe.to_string(index=False), actual_dataframe.to_string(index=False))
